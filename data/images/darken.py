74c6e86388559880fc92525e8a024f5ae20fa89d
Nick Wong <cupofpho@gmail.com>
2023-12-06 17:18:51 -0500

    Update for personal theme

--- data/images/darken.py
+++ data/images/darken.py
@@ -2,10 +2,10 @@
 import os
 
 pairs = [(f, f.replace(".svg", "_dark.svg")) 
-         for f in os.listdir(".") if f.endswith(".svg")]
+         for f in os.listdir(".") if f.endswith(".svg") and "_dark" not in f]
 
 for (l, d) in pairs:
     with open(l, "r") as fl, open(d,"w") as fd:
-        fd.write(fl.read().replace("#000000", "#a0a0a0"))
+        fd.write(fl.read().replace("#000000", "#eff1f5"))
     os.system(f"inkscape {d} -o {d.replace('.svg', '.png')}")
 
